d785aa16998b646f0fe0f7c5922f020588f99d78
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-09-16 19:55:28 +0200

    Added basic setup

---
+++ src/Sleddog.BigRedButton/BigRedButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using HidLibrary;
+
+namespace Sleddog.BigRedButton
+{
+    public class BigRedButton
+    {
+        private static readonly int VendorId = 0x1D34;
+        private static readonly int ProductId = 0x000D;
+        private IHidDevice device;
+
+        public BigRedButton()
+        {
+            var hidEnumerator = new HidEnumerator();
+
+            device = hidEnumerator.Enumerate(VendorId, ProductId).FirstOrDefault();
+        }
+    }
+}
